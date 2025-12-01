9ed7ff6d0e940a51a48e57131be88bbddb2f4d6a
CristoJV <44086220+CristoJV@users.noreply.github.com>
2025-12-01 16:03:21 +0100

    perf(day01): Use Modulo operator

--- aoc_25/src/day01.rs
+++ aoc_25/src/day01.rs
@@ -9,60 +9,39 @@ pub fn run() {
 fn part1(input: &str) -> i32 {
     let lines = input.lines();
     let mut dial: i32 = 50;
-    let mut count: u32 = 0;
-    let max_size: i32 = 100;
+    let mut password: i32 = 0;
+
     for line in lines{
         let direction = line.get(0..1).unwrap_or("V");
         let steps: i32 =line.get(1..).unwrap_or("0").parse().unwrap_or(0);
-        if direction == "R"{
-            dial = (dial + steps) % max_size;
-        }
-        else if direction == "L"{
-            dial = dial - steps;
-            while dial < 0{
-                dial = max_size + dial;
-            }
-        }
-        if dial == 0{
-            count +=1;
-        }
+        
+        // It is not necessary to apply modulo operator in each step.
+        // Just apply it on the total count.
+
+        dial += if direction=="L" {-steps} else {steps};
+        password +=i32::from(dial % 100 == 0);
 
     };
-    count as i32
+    password
 }
 
 fn part2(input: &str) -> i32 {let lines = input.lines();
     let mut dial: i32 = 50;
-    let mut count: i32 = 0;
-    let max_size: i32 = 100;
+    let mut password: i32 = 0;
 
     for line in lines{
         let direction = line.get(0..1).unwrap_or("V");
         let steps: i32 =line.get(1..).unwrap_or("0").parse().unwrap_or(0);
+        let delta = if direction=="L" {-steps} else {steps};
 
-        if direction == "R"{
-            let addition: i32 = dial + steps;
-            let over = addition / (max_size);
-            dial = (dial + steps) % max_size;
-            if over > 0 {
-                count = count + if dial >0 {over} else if dial==0 {over -1} else {0};
-            }
+        if delta >=0{
+            password += (dial + steps) / 100;
         }
-        else if direction == "L"{
-            let is_zero = if dial == 0 {1} else {0};
-            dial = dial - steps;
-            let mut on_top = 0;
-            while dial < 0{
-                dial = max_size + dial;
-                on_top +=1;
-            }
-            count += on_top - is_zero;
+        else{
+            let reversed  = (100 - dial) % 100;
+            password += (reversed - delta) / 100;
         }
-
-        if dial == 0{
-            count +=1;
-        }
-
+        dial = (dial + delta).rem_euclid(100);
     };
-    count as i32
+    password
 }
