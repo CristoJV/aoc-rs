97eb8d9dcbbc0e0dd42ac0fcd7d3d0139fea0dc2
CristoJV <44086220+CristoJV@users.noreply.github.com>
2025-12-03 19:34:26 +0100

    refactor(day03): Use the same recursive function for both parts

--- aoc_25/src/day03.rs
+++ aoc_25/src/day03.rs
@@ -6,28 +6,6 @@ pub fn run() {
     println!("Part 2: {}", part2(&input));
 }
 
-fn part1(input: &str) -> u32 {
-    let lines = input.lines();
-    let mut total_joltage: u32 = 0;
-    for line in lines{
-        let joltages: Vec<u32> = line.chars().into_iter().map(|x| x.to_digit(10).unwrap()).collect();
-        let mut max_joltages = vec![0,0];
-        let n = joltages.len();
-        for i in 0..n{
-            let joltage = joltages[i];
-            if i < (n-1) && joltage > max_joltages[0]  {
-                max_joltages[0] = joltage;
-                max_joltages[1] = 0;
-            }else if joltage > max_joltages[1]{
-                max_joltages[1] = joltage
-            };
-        }
-        let joltage = max_joltages.iter().fold(0, |acc, elem| acc * 10 +elem);
-        total_joltage +=joltage;
-    }
-    total_joltage
-}
-
 fn update(next_joltage: u64, max_joltages: &mut Vec<u64>, depth: usize, remaining_joltages:u64){
     let len = max_joltages.len();
     if depth >= len  {return}
@@ -40,6 +18,23 @@ fn update(next_joltage: u64, max_joltages: &mut Vec<u64>, depth: usize, remainin
         update(next_joltage, max_joltages, depth+1, remaining_joltages);
     }
 }
+
+fn part1(input: &str) -> u64 {
+    let lines = input.lines();
+    let mut total_joltage: u64 = 0;
+    for line in lines{
+        let joltages: Vec<u64> = line.chars().into_iter().map(|x| x.to_digit(10).unwrap() as u64).collect();
+        let mut max_joltages = vec![0,0];
+        let n = joltages.len();
+        for i in 0..n{
+            update(joltages[i], &mut max_joltages, 0, (n as u64)-(i as u64));
+        }
+        let joltage = max_joltages.iter().fold(0, |acc, elem| acc * 10 +elem);
+        total_joltage +=joltage;
+    }
+    total_joltage
+}
+
 fn part2(input: &str) -> u64 {let lines = input.lines();
     let mut total_joltage: u64 = 0;
     for line in lines{
